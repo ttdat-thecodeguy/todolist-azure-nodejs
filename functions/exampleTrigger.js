fe63a4fe9f0dfcb8cadf62dbbf5edd2864207581
Full Name <ttdat8@tma.com.vn>
2025-08-14 14:03:34 +0700

    first commit

--- functions/exampleTrigger.js
+++ functions/exampleTrigger.js
@@ -1,10 +1,32 @@
-const { app } = require('@azure/functions');
+const { app, output } = require('@azure/functions');
 
+const sendToQueue = output.storageQueue({
+  queueName: 'outqueue',
+  connection: 'AzureWebJobsStorage',
+});
 
-app.http("helloWorld1", {
-  methods: ["POST", "GET"],
+app.http('HttpExample', {
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  extraOutputs: [sendToQueue],
   handler: async (request, context) => {
-    context.log("Http function was triggered.");
-    return { body: "Hello, world!" };
+    try {
+      context.log(`Http function processed request for url "${request.url}"`);
+
+      const name = request.query.get('name') || (await request.text());
+      context.log(`Name: ${name}`);
+
+      if (name) {
+        const msg = `Name passed to the function ${name}`;
+        context.extraOutputs.set(sendToQueue, [msg]);
+        return { body: msg };
+      } else {
+        context.log('Missing required data');
+        return { status: 404, body: 'Missing required data' };
+      }
+    } catch (error) {
+      context.log(`Error: ${error}`);
+      return { status: 500, body: 'Internal Server Error' };
+    }
   },
 });
